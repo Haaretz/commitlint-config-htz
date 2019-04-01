4bbaa521b2137ad174ac85648a9779de672cf134
TxHawks <tofu.hawks@gmail.com>
2019-04-01 17:51:14 +0300

    feat: remove deprecated rules
    Remove the deprecated `lang` and `subject-tense` rules

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ module.exports = {
     'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 100],
-    lang: [1, 'always', 'eng'],
     'scope-case': [2, 'always', 'lowerCase'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'subject-tense': [1, 'always', ['present-imperative']],
     'type-empty': [1, 'never'],
     'type-enum': [
       2,
